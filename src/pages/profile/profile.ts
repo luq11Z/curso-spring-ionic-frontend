dff7605fba461da3e0c40fe9a98f77b6ed3c1c85
luq11Z <lucas_segundinho@hotmail.com>
2021-04-20 22:24:18 +0100

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -27,6 +27,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -68,6 +72,19 @@ export class ProfilePage {
      this.camaraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(response => {
+      this.picture = null; 
+      this.loadData();
+    },
+    error=> {});
+  }
+
+  cancel() {
+    this.picture = null;
   }
 
 }
